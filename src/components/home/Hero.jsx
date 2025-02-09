43d1f97545a8dec9fbb89a92896d21fe37fe7195
romeoscript <romeobourne211@gmail.com>
2025-02-09 14:00:41 +0100

    drug abuse

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { Sparkles } from 'lucide-react';
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
-import ai from '../../assets/Ai.svg';
+import ai from '../../assets/AI.svg';
 
 const Hero = ({ onGetStarted }) => {
   const particlesInit = useCallback(async engine => {
